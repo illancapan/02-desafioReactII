6db0206a3aa898a49ac9991a9fb2223bbee2dc2f
illancapan <igorllancapan@gmail.com>
2024-04-19 01:25:08 -0400

    add: icon, title

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,19 +1,25 @@
+import IconHeart from './IconHeart'
 import { PhotoContext } from "../context/PhotoContext";
 import { useContext } from "react";
 
 const Gallery = () => {
-  
-  const { photos } = useContext(PhotoContext)
+  const { photos } = useContext(PhotoContext);
 
   return (
-    // <PhotoContext.Provider>
-      <div className="gallery grid-columns-5 p-3">
-        {Array.isArray(photos) &&
-          photos.map((photo, index) => (
-            <img key={index} src={photo.url} alt={`Photo ${index}`} />
-          ))}
-      </div>
-    // </PhotoContext.Provider>
+    <div className="gallery grid-columns-5 p-3">
+      {Array.isArray(photos) &&
+        photos.map((photo, index) => (
+          <div key={index} className="photo-container">
+            <div className="image-wrapper">
+              <img src={photo.url} alt={`Photo ${index}`} />
+              <div className="icon-heart-container">
+                <IconHeart filled={photo.liked} />
+              </div>
+              <p className="photo-title">{photo.alt}</p>
+            </div>
+          </div>
+        ))}
+    </div>
   );
 };
 
--- src/views/Favorites.jsx
+++ src/views/Favorites.jsx
@@ -1,8 +1,11 @@
+import Gallery from "../components/Gallery";
+
 const Favorites = () => {
   return (
     <div>
       <h1>Fotos favoritas</h1>
       <div className="p-3 gallery grid-columns-4"></div>
+      <Gallery />
     </div>
   );
 };
