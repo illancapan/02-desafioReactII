df5ebb5f0a31a61269265e85e0f46bf41ec6827f
illancapan <igorllancapan@gmail.com>
2024-04-19 02:20:03 -0400

    entregable

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,9 +1,18 @@
-import IconHeart from './IconHeart'
-import { PhotoContext } from "../context/PhotoContext";
 import { useContext } from "react";
+import IconHeart from "./IconHeart";
+import { PhotoContext } from "../context/PhotoContext";
 
 const Gallery = () => {
-  const { photos } = useContext(PhotoContext);
+  const { photos, setPhotos } = useContext(PhotoContext);
+
+  const handleLikeClick = (id) => {
+    // encuentra la foto en el array de fotos
+    const updatedPhotos = photos.map((photo) =>
+      photo.id === id ? { ...photo, liked: !photo.liked } : photo
+    );
+    // actualiza el array de fotos con la foto marcada como favorita
+    setPhotos(updatedPhotos);
+  };
 
   return (
     <div className="gallery grid-columns-5 p-3">
@@ -13,7 +22,7 @@ const Gallery = () => {
             <div className="image-wrapper">
               <img src={photo.url} alt={`Photo ${index}`} />
               <div className="icon-heart-container">
-                <IconHeart filled={photo.liked} />
+                <IconHeart filled={photo.liked} onLikeClick={() => handleLikeClick(photo.id)} />
               </div>
               <p className="photo-title">{photo.alt}</p>
             </div>
--- src/views/Favorites.jsx
+++ src/views/Favorites.jsx
@@ -1,12 +1,32 @@
-import Gallery from "../components/Gallery";
+
+import { useContext } from "react";
+import { PhotoContext } from "../context/PhotoContext";
+import IconHeart from '../components/IconHeart'
 
 const Favorites = () => {
+  const { photos } = useContext(PhotoContext);
+
+  // filtrar las fotos para mostrar marcadas como favoritas
+  const favoritePhotos = photos.filter((photo) => photo.liked);
+
   return (
     <div>
       <h1>Fotos favoritas</h1>
-      <div className="p-3 gallery grid-columns-4"></div>
-      <Gallery />
+      <div className="p-3 gallery grid-columns-4">
+        {favoritePhotos.map((photo) => (
+          <div key={photo.id} className="photo-container">
+            <div className="image-wrapper">
+              <img src={photo.url} alt={photo.alt} />
+              <div className="icon-heart-container">
+                <IconHeart id={photo.id} filled={photo.liked} />
+              </div>
+              <p className="photo-title">{photo.alt}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
+
 export default Favorites;
